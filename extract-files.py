9d532be586bbbc73726ea1866685b2b47a5bdcca
mikeNG <mikeNG@users.noreply.github.com>
2026-07-02 07:15:23 +0000

    alioth: power-libperfmgr: Update included soong namespaces
    We're putting everything that depends on pixelatoms behind a soong
    namespace, since hardware/google/pixel is no longer available after
    CP2A. Using a combination of prebuilts and source built things that
    depend on pixelatoms makes the build fail catastrophically, so we
    will just make everything prebuilt for supported Pixel devices.
    
    Change-Id: I3d9e1d9436f1edfbb45bca0aa709334287bb12ce
    Signed-off-by: pabloescobar-reborn <yogivangala07@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -67,6 +67,11 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 namespace_imports = [
+    'hardware/google/interfaces',
+    'hardware/google/pixel',
+    'hardware/google/pixel/pixelstats',
+    'hardware/google/pixel/power-libperfmgr',
+    'hardware/lineage/interfaces/power-libperfmgr',
     'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8250',
     'vendor/qcom/opensource/display',
